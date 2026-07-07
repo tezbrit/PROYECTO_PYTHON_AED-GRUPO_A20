c0e62a12b0a53c219f440dff6d5df9f212b1910c
tezbrit <octabrittez@gmail.com>
2026-07-07 18:16:31 -0300

    Se agrega función para cargar habitaciones y para guardar habitaciones a partir del archivo de texto previamente creado

--- hotel.py
+++ hotel.py
@@ -15,6 +15,40 @@ def habitaciones_por_defecto():
         {"numero": 302, "tipo": "Suite",  "precio": 45000, "estado": "disponible", "dni": ""},
     ]
 
+def cargar_habitaciones():
+    habitaciones = []
+
+    # Si el archivo no existe, es la primera vez que se abre el sistema
+    if not os.path.exists(ARCHIVO_HABITACIONES):
+        habitaciones = habitaciones_por_defecto()
+        guardar_habitaciones(habitaciones)
+        return habitaciones
+
+    # Si el archivo existe, leemos línea por línea
+    archivo = open(ARCHIVO_HABITACIONES, "r", encoding="utf-8")
+    for linea in archivo:
+        linea = linea.strip()
+        if linea != "":
+            partes = linea.split(",")
+            habitacion = {
+                "numero": int(partes[0]),
+                "tipo":   partes[1],
+                "precio": int(partes[2]),
+                "estado": partes[3],
+                "dni":    partes[4]
+            }
+            habitaciones.append(habitacion)
+    archivo.close()
+
+    return habitaciones
+
+def guardar_habitaciones(habitaciones):
+    archivo = open(ARCHIVO_HABITACIONES, "w", encoding="utf-8")
+    for h in habitaciones:
+        linea = str(h["numero"]) + "," + h["tipo"] + "," + str(h["precio"]) + "," + h["estado"] + "," + h["dni"] + "\n"
+        archivo.write(linea)
+    archivo.close()
+
 def main():
     print("Bienvenido al Sistema de Gestion de Hotel!")
 
